953e5192ca6faed4128bed525dd852c90b123937
shin32125 <shintaga32123@gmail.com>
2024-05-25 12:57:23 +0900

    p

--- evolution_Spacewar.mk
+++ evolution_Spacewar.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_Spacewar
+PRODUCT_NAME := evolution_Spacewar
 PRODUCT_DEVICE := Spacewar
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A063
