a307f0eeae35f47c696d00639d782d470b26a9ee
shin32125 <shintaga32123@gmail.com>
2024-11-25 12:03:32 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 #
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
