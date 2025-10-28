9362414f21df338862d69bf6a4285e8d71e37edc
shin32125 <shintaga32123@gmail.com>
2025-10-28 15:50:36 +0900

    init.kernel.post_boot.sh の更新

--- rootdir/bin/init.kernel.post_boot.sh
+++ rootdir/bin/init.kernel.post_boot.sh
@@ -125,6 +125,7 @@ function configure_memory_parameters() {
 
 	configure_zram_parameters
 	configure_read_ahead_kb_values
+ echo 1 > /sys/block/zram1/reset
  echo lz4 > /sys/block/zram0/comp_algorithm
 	echo 100 > /proc/sys/vm/swappiness
 
